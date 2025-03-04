0357a45a584fee7112f400dea2152493421d3c2a
MarioCamayo <mariocamayo17@gmail.com>
2025-03-04 18:58:27 -0500

    se desarrollo  un efecto de carga, desenfocado con un boton de click

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('Hello!');
+window.addEventListener('load', ()=>{
+    
+    //Seleccionar elementos del Dom
+    let number = document.querySelector('.load__number')
+    let btn = document.querySelector('.load__btn')
+    let complete = document.querySelector('.load__complete')
+    let background = document.querySelector('.main__background')
+
+    //Variables contadores
+
+    let blur = 30
+    let percent = 1
+
+    btn.addEventListener('click', (event)=>{
+
+        btn.style.display ='none'
+
+        let inter  = setInterval(()=>{
+            percent++
+            blur -= 30/100
+
+            if(percent > 100){
+                clearInterval(inter)
+                complete.style.display = 'block'
+            }else{
+                number.innerHTML = percent+'%'
+                background.style.filter = `blur(${blur}px)`
+            }
+
+        }, 20)
+    })
+})
